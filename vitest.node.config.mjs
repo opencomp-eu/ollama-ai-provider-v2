3f6339ca6144664eb5cc9de7b6bcbcbbd29e9a42
Thomas Nordentoft <thomas@nordentoft.dk>
2026-06-11 12:02:45 +0200

    Remove coverage

--- vitest.node.config.mjs
+++ vitest.node.config.mjs
@@ -6,14 +6,5 @@ export default defineConfig({
     environment: 'node',
     globals: true,
     include: ['**/*.test.ts', '**/*.test.tsx'],
-    coverage: {
-      provider: 'v8',
-      include: ['src/**/*.ts'],
-      exclude: [
-        'src/**/*.test.ts',
-        'src/**/test-helpers/**',
-        'src/test-utils/**',
-      ],
-    },
   },
 });
